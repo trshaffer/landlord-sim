97c05e501b822afd1356388c198a82ea1ea7ac75
Thanh Son Phung <tphung@nd.edu>
2022-06-14 12:52:55 -0400

    done naive agnes

---
+++ MVC.py
@@ -0,0 +1,11 @@
+class MVC:
+    def __init__(self):
+        pass
+    def add_point(self, p):
+        pass
+    def cluster(self):
+        pass
+
+class MVC_Point:
+    def __init__(self):
+        pass
